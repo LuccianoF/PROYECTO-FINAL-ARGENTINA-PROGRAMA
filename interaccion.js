7be32fb1444619161c005ce9917915224ff93f44
Luciano Falcone <luccianofalcone@gmail.com>
2023-02-28 20:37:55 -0300

    Entrega final CV

---
+++ interaccion.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('click', function() {
+    console.log("Estoy haciendo mi primer proyecto de front-end");
+    document.getElementById("enviar").innerHTML = "Aguarde un instante"
+});
+
+document.addEventListener('click', function() {
+    console.log("Te estoy mandando a otra seccion");
+    document.getElementById("links-navegacion").innerHTML = "Redirigiendo"
+});
+
+
+
+
